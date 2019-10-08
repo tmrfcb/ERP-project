d8f005b3c981812878af73edafd809a03d63045d
Riadh Troudi <riadh.troudi-ext@yrnet.com>
2019-10-08 17:43:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateUser from "./components/users/create-user.component";
+import EditUser from "./components/users/edit-user.component";
+import UsersList from "./components/users/users-list.component";
+import logo from "./logo.svg";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">ERP</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Users</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create User</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={UsersList} />
+          <Route path="/edit/:id" component={EditUser} />
+          <Route path="/create" component={CreateUser} />
+        </div>
+      </Router>
+    );
+  }
+}
+export default App;
---
+++ src/components/users/edit-user.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class EditUser extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Edit Todo Component!!</p>
+            </div>
+        )
+    }
+}
