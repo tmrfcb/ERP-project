076be7427e1742cb0b960dc80a61c80df37efea0
Riadh Troudi <riadh.troudi-ext@yrnet.com>
2019-11-15 12:43:00 +0100

    mise en place v2

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,22 @@ const PORT = 4000;
 let User = require('./user.model');
 
 
-var whitelist = ['http://localhost:3000']
+var whitelist = ['http://localhost:3000'];
 var corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+   /* if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORScc'))
     }
+    */
+   callback(null, true)
   }
-}
+}  
 app.use(cors(corsOptions));
 
 app.use('/users', userRoutes);
-
+ 
 
 
 app.use(bodyParser.json());
@@ -29,7 +31,7 @@ app.use(bodyParser.json());
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/erp', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/erp', { useNewUrlParser: true , useUnifiedTopology: true});
 const connection = mongoose.connection;
 
 connection.once('open', function() {
@@ -38,12 +40,12 @@ connection.once('open', function() {
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
-});
+}); 
 
 userRoutes.route('/').get(function(req, res) {
     User.find(function(err, Users) {
         if (err) {
-            console.log(err);
+            console.log(err); 
         } else {
             res.json(Users);
         }
@@ -61,7 +63,7 @@ userRoutes.route('/add').post(function(req, res) {
     let user = new User(req.body);
     user.save()
         .then(user => {
-            res.status(200).json({'todo': 'user added successfully'});
+            res.status(200).json({'user': 'user added successfully'});
         })
         .catch(err => {
             res.status(400).send('adding new user failed');
@@ -73,13 +75,14 @@ userRoutes.route('/update/:id').post(function(req, res) {
         if (!user)
             res.status(404).send("data is not found");
         else
+        
             user.user_description = req.body.user_description;
             user.user_responsible = req.body.user_responsible;
             user.user_priority = req.body.user_priority;
             user.user_completed = req.body.user_completed;
 
             user.save().then(user => {
-                res.json('Todo updated!');
+                res.json('user updated!');
             })
             .catch(err => {
                 res.status(400).send("Update not possible");
--- src/components/users/edit-user.component.js
+++ src/components/users/edit-user.component.js
@@ -1,9 +1,156 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 export default class EditUser extends Component {
+
+    constructor(props) {
+        super(props);  
+
+        this.onChangeUserDescription = this.onChangeUserDescription.bind(this);
+        this.onChangeUserResponsible = this.onChangeUserResponsible.bind(this);
+        this.onChangeUserPriority = this.onChangeUserPriority.bind(this);
+        this.onChangeUserCompleted = this.onChangeUserCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            user_description: '',
+            user_responsible: '',
+            user_priority: '',
+            user_completed: false
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/users/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    user_description: response.data.user_description,
+                    user_responsible: response.data.user_responsible,
+                    user_priority: response.data.user_priority,
+                    user_completed: response.data.user_completed
+                })   
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    onChangeUserDescription(e) {
+        this.setState({
+            user_description: e.target.value
+        });
+    }
+
+    onChangeUserResponsible(e) {
+        this.setState({
+            user_responsible: e.target.value
+        });
+    }
+
+    onChangeUserPriority(e) {
+        this.setState({
+            user_priority: e.target.value
+        });
+    }
+
+    onChangeUserCompleted(e) {
+        this.setState({
+            user_completed: !this.state.user_completed
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+            user_description: this.state.user_description,
+            user_responsible: this.state.user_responsible,
+            user_priority: this.state.user_priority,
+            user_completed: this.state.user_completed
+        };
+        console.log(obj);
+        axios.post('http://localhost:4000/users/update/'+this.props.match.params.id, obj)
+            .then(res => console.log(res.data));
+        
+        this.props.history.push('/');
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to Edit Todo Component!!</p>
+                <h3 align="center">Update User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <label>Description: </label>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.user_description}
+                                onChange={this.onChangeUserDescription}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.user_responsible}
+                                onChange={this.onChangeUserResponsible}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityLow" 
+                                    value="Low"
+                                    checked={this.state.user_priority==='Low'} 
+                                    onChange={this.onChangeUserPriority}
+                                    />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityMedium" 
+                                    value="Medium" 
+                                    checked={this.state.user_priority==='Medium'} 
+                                    onChange={this.onChangeUserPriority}
+                                    />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" 
+                                    type="radio" 
+                                    name="priorityOptions" 
+                                    id="priorityHigh" 
+                                    value="High" 
+                                    checked={this.state.user_priority==='High'} 
+                                    onChange={this.onChangeUserPriority}
+                                    />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-check">
+                        <input  className="form-check-input"
+                                id="completedCheckbox"
+                                type="checkbox"
+                                name="completedCheckbox"
+                                onChange={this.onChangeUserCompleted}
+                                checked={this.state.user_completed}
+                                value={this.state.user_completed}
+                                />
+                        <label className="form-check-label" htmlFor="completedCheckbox">
+                            Completed
+                        </label>                        
+                    </div>
+
+                    <br />
+
+                    <div className="form-group">
+                        <input type="submit" value="Update User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
